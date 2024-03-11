5fac9548a3961ee6d0d999236141a347e99f08a0
Marvin Jens <marvin.jens@mdc-berlin.de>
2024-03-11 15:26:44 +0100

    * initial plumbing of setup code. Can build sdist for upload to pypi

---
+++ scbamtools/contrib.py
@@ -0,0 +1,4 @@
+__version__ = 0.1
+__author__ = ["Marvin Jens"]
+__email__ = ["marvin.jens@charite.de"]
+__license__ = "MIT"
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, Extension
+#from Cython.Build import cythonize
+#from Cython.Compiler import Options
+# import numpy
+
+# These are optional
+#Options.docstrings = True
+#Options.annotate = False
+
+# Modules to be compiled and include_dirs when necessary
+#extensions = [
+#    Extension(
+#        "mrfifo.fast_loops",
+#        ["mrfifo/fast_loops.pyx"], #include_dirs=[numpy.get_include()],
+#    ),
+    # Extension(
+    #     "spacemake.cython.fastread",
+    #     ["spacemake/cython/fastread.pyx"], include_dirs=[numpy.get_include()],
+    # ),
+
+#]
+
+if __name__ == "__main__":
+    from setuptools import setup
+
+    setup(
+        name='scbamtools',  # Required
+
+        # A list of compiler Directives is available at
+        # https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#compiler-directives
+
+        # external to be compiled
+#        ext_modules = cythonize(extensions, compiler_directives={"language_level": 3, "profile": False}),
+    )
