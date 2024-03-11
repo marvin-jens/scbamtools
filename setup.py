089102665b0b262ac8913f9e11446c339194c2b3
Marvin Jens <marvin.jens@mdc-berlin.de>
2024-03-11 15:36:47 +0100

    * add some existing code from spacemake

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, Extension
-#from Cython.Build import cythonize
-#from Cython.Compiler import Options
-# import numpy
+from Cython.Build import cythonize
+from Cython.Compiler import Options
+import numpy
 
 # These are optional
-#Options.docstrings = True
-#Options.annotate = False
+Options.docstrings = True
+Options.annotate = False
 
 # Modules to be compiled and include_dirs when necessary
-#extensions = [
-#    Extension(
-#        "mrfifo.fast_loops",
-#        ["mrfifo/fast_loops.pyx"], #include_dirs=[numpy.get_include()],
-#    ),
-    # Extension(
-    #     "spacemake.cython.fastread",
-    #     ["spacemake/cython/fastread.pyx"], include_dirs=[numpy.get_include()],
-    # ),
+extensions = [
+    Extension(
+        "scbamtools.cython.kmers",
+        ["scbamtools/cython/kmers.pyx"], #include_dirs=[numpy.get_include()],
+    ),
+   Extension(
+        "scbamtools.cython.bctree",
+        ["scbamtools/cython/bctree.pyx"], include_dirs=[numpy.get_include()],
+   ),
 
-#]
+]
 
 if __name__ == "__main__":
     from setuptools import setup
@@ -30,5 +30,5 @@ if __name__ == "__main__":
         # https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#compiler-directives
 
         # external to be compiled
-#        ext_modules = cythonize(extensions, compiler_directives={"language_level": 3, "profile": False}),
+        ext_modules = cythonize(extensions, compiler_directives={"language_level": 3, "profile": False}),
     )
