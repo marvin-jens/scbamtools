8874a931a16eb6d839fadab65b271792fa91ff9f
Marvin Jens <marvin.jens@charite.de>
2024-05-17 15:33:55 +0200

    * deactivate log file by default

--- scbamtools/util.py
+++ scbamtools/util.py
@@ -62,8 +62,8 @@ def make_minimal_parser(prog="", usage="", **kw):
     parser = argparse.ArgumentParser(prog=prog, usage=usage, **kw)
     parser.add_argument(
         "--log-file",
-        default=f"{prog}.log",
-        help=f"place log entries in this file (default={prog}.log)",
+        default="",  # f"{prog}.log",
+        help=f"place log entries in this file (default=off)",
     )
     parser.add_argument(
         "--log-level",
